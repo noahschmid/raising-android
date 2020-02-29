97f8a2c62db86ac796321e413bbe5639a8b932f9
noahschmid <noah.schmid77@gmail.com>
2020-02-29 14:09:13 +0100

    Add basic registration logic

--- app/src/main/java/com/example/prototypeapplication/PrototypeRegisterFragment.java
+++ app/src/main/java/com/example/prototypeapplication/PrototypeRegisterFragment.java
@@ -8,9 +8,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PrototypeRegisterFragment extends Fragment {
 
@@ -31,6 +40,41 @@ public class PrototypeRegisterFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(PrototypeRegisterViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private void register(String username, String password) {
+        try {
+            String endpoint = "http://33383.hostserv.eu:8080/account/register";
+            JSONObject params = new JSONObject();
+            params.put("username", username);
+            params.put("password", password);
+            JsonObjectRequest loginRequest = new JsonObjectRequest(Request.Method.POST,
+                    endpoint,
+                    params,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            //TODO: display success
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    if(error.networkResponse.statusCode == 403) {
+                        String body = new String(error.networkResponse.data);
+                        try {
+                            JSONObject response = new JSONObject(body);
+                            //TODO: display error
+                        } catch (JSONException e) {
+                            Log.d("debugMessage", e.toString());
+                        }
+                    }
+                    Log.d("debugMessage", error.toString());
+                }
+            });
+            ApiRequestHandler.getInstance(getContext()).addToRequestQueue(loginRequest);
+        } catch(Exception e) {
+            Log.d("debugMessage", e.getMessage());
+        }
     }
 
 }
