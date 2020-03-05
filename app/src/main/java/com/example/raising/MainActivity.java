e8de3b50cdf76a99cd497c2bb7b5ba6712e25273
Lorenz Caliezi <lorenz.caliezi@students.unibe.ch>
2020-03-05 16:28:36 +0100

    Add functionality of navigation bar

--- app/src/main/java/com/example/raising/MainActivity.java
+++ app/src/main/java/com/example/raising/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.raising;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.raising.authentication.fragments.LoginFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +19,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        /*
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
         fragmentTransaction.add(R.id.fragment_container, new LoginFragment());
         fragmentTransaction.commit();
+
+         */
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selected = null;
+
+                    switch(item.getItemId()) {
+                        case R.id.nav_home:
+                            selected = new MatchesFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selected = new ProfileFragment();
+                            break;
+                        case R.id.nav_settings:
+                            selected = new SettingsFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, selected)
+                            .commit();
+
+                    return true;
+                }
+            };
 }
