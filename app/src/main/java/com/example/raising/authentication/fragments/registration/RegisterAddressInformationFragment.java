aebca4e170a7a0dd9a463bbd507a6073171182df
Lorenz Caliezi <lorenz.caliezi@students.unibe.ch>
2020-03-11 16:13:32 +0100

    Add address information fragment

--- app/src/main/java/com/example/raising/authentication/fragments/registration/RegisterAddressInformationFragment.java
+++ app/src/main/java/com/example/raising/authentication/fragments/registration/RegisterAddressInformationFragment.java
@@ -2,16 +2,21 @@ package com.example.raising.authentication.fragments.registration;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.raising.R;
 import com.example.raising.RaisingFragment;
 
 public class RegisterAddressInformationFragment extends RaisingFragment implements View.OnClickListener {
+    private EditText addressCountryInput, addressCityInput, addressZipInput,
+            addressStreetInput, addressWebsiteInput;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +29,20 @@ public class RegisterAddressInformationFragment extends RaisingFragment implemen
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        addressCountryInput = view.findViewById(R.id.register_input_address_country);
+        addressCityInput = view.findViewById(R.id.register_input_address_city);
+        addressZipInput = view.findViewById(R.id.register_input_address_zip);
+        addressStreetInput = view.findViewById(R.id.register_input_address_street);
+        addressWebsiteInput = view.findViewById(R.id.register_input_address_website);
+
+        Button btnAddressInformation = view.findViewById(R.id.button_address_information);
+        btnAddressInformation.setOnClickListener(this);
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
 
@@ -33,6 +52,11 @@ public class RegisterAddressInformationFragment extends RaisingFragment implemen
     @Override
     public void onClick(View view) {
         switch(getId()) {
+            case R.id.button_address_information:
+                //TODO: insert function to be executed
+                break;
+            default:
+                break;
         }
     }
 }
